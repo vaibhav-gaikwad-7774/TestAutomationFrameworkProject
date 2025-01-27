a04d29f2cc9b74e24658d1e75cff001947eca17b
Vaibhav Gaikwad <GFG19667@gfg.geeksforgeeks.org>
2025-01-27 18:06:22 +0530

    login test

--- src/main/java/io/learn/pages/LoginPage.java
+++ src/main/java/io/learn/pages/LoginPage.java
@@ -14,57 +14,33 @@ public class LoginPage {
     private By userNameField = By.id("user-name");
     private By passwordField = By.id("password");
     private By loginButton = By.id("login-button");
-    private By errorMessage = By.cssSelector(".error-message-container.error");
-    private By appLogoText = By.xpath("//div[@class='app_logo']");
+    
 
     public LoginPage(WebDriver driver) {
-        this.driver = driver;
-    }
-
-    // actions
-    public void enterCredentials(String username, String password) {
-        this.enterUsername(username);
-        this.enterPassword(password);
-        this.clickLogin();
-    }
-
-    public String getLogoText() {
-        return driver.findElement(appLogoText).getText();
-    }
-
-    private void enterUsername(String name) {
-        driver.findElement(userNameField).clear();
-        driver.findElement(userNameField).sendKeys(name);
-    }
-
-    private void enterPassword(String password) {
-        driver.findElement(passwordField).clear();
-        driver.findElement(passwordField).sendKeys(password);
+    	this.driver = driver;
     }
     
-    public void clickLogin() {
-    	driver.findElement(loginButton).click();
-    	}
-
-//    public ProductPage clickLogin() {
-//        driver.findElement(loginButton).click();
-//        if(isLoginErrorDisplayed()) {
-//            throw new LoginFailedException("Login failed due to incorrect username or password.");
-//        }
-//        return new ProductPage(driver);
-//    }
-
-    public boolean isLoginErrorDisplayed() {
-        try {
-            WebElement element = driver.findElement(errorMessage);
-            return element.isDisplayed();
-        } catch (NoSuchElementException e) {
-            // if the error message is not found, there was no error - login successful
-            return false;
-        }
+    //actions
+    
+    public void login(String username, String password) {
+    	this.enterUsername(username);
+    	this.enterPassword(password);
+    	this.clickLogin();
     }
-
-    public boolean isLoginPageDisplayed() {
-        return driver.findElement(userNameField).isDisplayed();
+    
+    
+    private void enterUsername(String name) {
+    	driver.findElement(userNameField).clear();
+    	driver.findElement(userNameField).sendKeys(name);
+    }
+    
+    private void enterPassword(String password) {
+    	driver.findElement(passwordField).clear();
+    	driver.findElement(passwordField).sendKeys(password);
+    }
+    
+    
+    private void clickLogin() {
+    	driver.findElement(loginButton).click();
     }
 }
--- src/test/java/io/learn/tests/LoginTest.java
+++ src/test/java/io/learn/tests/LoginTest.java
@@ -1,7 +1,25 @@
 package io.learn.tests;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import io.learn.pages.LoginPage;
+
 public class LoginTest {
-    public static void main(String[] args) {
+	
+	private WebDriver driver;
+	
+	@BeforeClass
+	public void setUp() {
+		driver = new ChromeDriver();
+		
+	}
+	
+	@Test
+	public void testLoginFlow() {
+		LoginPage loginPage = new LoginPage(driver);
 		
 	}
 }
